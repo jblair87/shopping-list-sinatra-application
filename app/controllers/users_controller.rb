37e642ce613394e52d3d1508e03d875d7406d230
Jenna Mandel <jenna.b.mandel@gmail.com>
2017-06-03 02:48:43 +0000

    added login and signup to users controller, add sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+
+get '/signup' do
+   if !logged_in?
+     erb :'users/new'
+    else
+      redirect to 'users/list'
+   end
 end
+
+post '/signup' do
+if params[:username] == "" || params[:password] == ""
+redirect to signup
+else
+  @user = User.new(:username => params[:username], :password => params[:password])
+session[:user_id] = @user.id
+    redirect 'user/list'
+end
+end
+get '/login' do
+   if !logged_in?
+     erb :'users/login'
+   else
+     redirect to 'users/list'
+   end
+ end
