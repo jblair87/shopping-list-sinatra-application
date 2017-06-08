6de72981e40d004c5d274980d8825787b181e442
Jenna Mandel <jenna.b.mandel@gmail.com>
2017-06-08 03:17:02 +0000

    added edit file to item view, added show files to user and item views, modified controllers, modified views for signing in and creating an account, creating an item and viewing item list

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+
   get '/items' do
     if logged_in?
       @items = Item.all
@@ -8,16 +9,70 @@ class ItemsController < ApplicationController
     end
   end
 
-  get 'items/create' do
-    erb :'items/create'
+  get '/items/create' do
+    if logged_in?
+      erb :'items/create'
+    else
+      redirect to '/login'
+    end
+  end
+
+  post '/items' do
+    if params.values.any? {|value| value == ""}
+     redirect to "/items/new"
+   else
+      user = User.find(session[:user_id])
+     @item = Item.create(name: params[:name], quantity: params[:quantity])
+    redirect to "/items/#{@item.id}"
+   end
+ end
+
+  get '/items/:id' do
+    if logged_in?
+      @item = Item.find(params[:id])
+      erb :'items/show'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/items/:id/edit' do
+    if logged_in?
+      @item = Item.find(params[:id])
+      if @item.user_id == session[:user_id]
+       erb :'items/edit'
+      else
+        redirect to '/items'
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
+  patch '/items/:id' do
+    if params[:name] == ""
+      redirect to "/items/#{params[:id]}/edit"
+    else
+      @item = Item.find(params[:id])
+      @item.name = params[:name]
+      @item.quantity = params[:quantity]
+      @item.save
+      redirect to "/items/#{@item.id}"
+    end
+  end
+
+  delete '/items/:id/delete' do
+      @item = Item.find(params[:id])
+      if session[:user_id]
+            @item = Item.find(params[:id])
+  if @project.user_id == session[:user_id]
+      @item.delete
+        redirect to '/items'
+      else
+        redirect to '/items'
+      end
+    else
+      redirect to '/login'
+    end
   end
 end
-
-post '/items' do
-    @items = Item.create(params[:item])
-    if !params[:item][:name].empty?
-     @Item.category = Item.create(name: params[:item][:name])
-     end
-     @item.save
-   redirect to "items/#{@item.id}"
-    end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,45 +1,54 @@
 class UsersController < ApplicationController
-  get '/users/:id' do
-      @user = User.find_by_id(params[:id])
-      erb :'/items/items'
-    end
+
+get "/signup" do
+  if !logged_in?
+  erb :'users/new'
+else
+  redirect to 'items'
+end
+end
+
+post '/signup' do
+if params[:username] == "" || params[:password] == ""
+ redirect to 'users/new'
+else
+   @user = User.new(:username => params[:username],  :password => params[:password])
+   @user.save
+   session[:user_id] = @user.id
+  redirect to '/items'
+end
+end
 
 
-    post '/signup' do
-   if params[:username] == "" || params[:password] == ""
-     redirect to '/signup'
-    else
-       @user = User.new(:username => params[:username], :password => params[:password])
-       @user.save
-       session[:user_id] = @user.id
-      redirect to '/items'
-    end
-   end
-
-
-    get "/signup" do
-      if !logged_in?
-        erb :'users/new'
-      else
-        redirect to '/items/items'
-  	end
-     end
 
 get '/login' do
-   if !logged_in?
-     erb :'index'
-   else
-     redirect to '/items/items'
-   end
- end
+  if !logged_in?
+  erb :'users/login'
+else
+  redirect to '/items'
+end
+end
 
 post '/login' do
    user = User.find_by(:username => params[:username])
    if user && user.authenticate(params[:password])
      session[:user_id] = user.id
-     redirect "/items/items"
+     redirect "/items"
    else
-     redirect to '/index'
+     redirect to 'users/login'
    end
  end
+
+
+ get '/logout' do
+   if logged_in?
+   session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
+ end
+ end
+
+
+
 end
