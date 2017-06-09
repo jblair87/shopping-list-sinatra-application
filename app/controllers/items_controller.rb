c3224da8f0fd3ceddcbc7455470b251bab18fdf9
Jenna Mandel <jenna.b.mandel@gmail.com>
2017-06-09 02:38:26 +0000

    rename items/create to items/new for consistency, update controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,9 +9,9 @@ class ItemsController < ApplicationController
     end
   end
 
-  get '/items/create' do
+  get '/items/new' do
     if logged_in?
-      erb :'items/create'
+      erb :'items/new'
     else
       redirect to '/login'
     end
