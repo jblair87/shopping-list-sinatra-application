2121bff96576cd49d002b1dd7c903ab0b46bc6b4
Jenna Mandel <jenna.b.mandel@gmail.com>
2017-06-10 22:23:24 +0000

    add user id column to categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170610221347) do
+ActiveRecord::Schema.define(version: 20170610222119) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.integer "user_id"
   end
 
   create_table "items", force: :cascade do |t|
